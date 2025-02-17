3b6ece87e85a5f74fa9ca20822252b05977b2b2d
YourMJK <developer@mjkrempl.com>
2025-02-17 13:09:37 +0100

    Changed cleanup task to only run while necesarry

--- src/main/java/com/mjkrempl/cartchunkloader/ChunkManagement/WorldChunkManager.java
+++ src/main/java/com/mjkrempl/cartchunkloader/ChunkManagement/WorldChunkManager.java
@@ -3,6 +3,7 @@ package com.mjkrempl.cartchunkloader.ChunkManagement;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.*;
 
@@ -12,40 +13,29 @@ public final class WorldChunkManager {
 	private final JavaPlugin plugin;
 	private final World world;
 	private final int regionRadius;
+	private final int inactiveRegionLoadTime;
+	private final int updateInterval;
 	private final Map<UUID, ChunkCoord> entityActiveRegions;
 	private final Map<UUID, Long> entityActiveTimes;
 	private final Multiset<ChunkCoord> chunkTickets;
+	private BukkitTask cleanupTask;
 	
 	public WorldChunkManager(JavaPlugin plugin, World world, int regionRadius, int inactiveRegionLoadTime, int updateInterval) {
 		this.plugin = plugin;
 		this.world = world;
 		this.regionRadius = regionRadius;
+		this.inactiveRegionLoadTime = inactiveRegionLoadTime;
+		this.updateInterval = updateInterval;
 		this.entityActiveRegions = new HashMap<>();
 		this.entityActiveTimes = new HashMap<>();
 		this.chunkTickets = HashMultiset.create();
-		
-		// Setup periodic runnable to unload each entity's last active region after `inactiveRegionLoadTime` of inactivity
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				long now = world.getGameTime();
-				
-				// Enumerate previously active entities and the time of their last activity
-				entityActiveTimes.entrySet().removeIf(entry -> {
-					long entityActiveTime = entry.getValue();
-					if ((now - entityActiveTime) < inactiveRegionLoadTime) return false;
-					
-					// Entity was inactive for longer than `inactiveRegionLoadTime`, remove last active region and forget entity
-					ChunkCoord coord = entityActiveRegions.remove(entry.getKey());
-					removeRegion(coord);
-					return true;
-				});
-			}
-		}.runTaskTimer(plugin, updateInterval, updateInterval);
 	}
 	
 	
 	public void onEntityActivity(UUID entityUID, int x, int z) {
+		// Restart cleanup task again if this is the first active entity
+		restartCleanupTask();
+		
 		// Update last active time for entity
 		entityActiveTimes.put(entityUID, world.getGameTime());
 		
@@ -118,5 +108,34 @@ public final class WorldChunkManager {
 				return coord;
 			}
 		};
+	}
+	
+	private void restartCleanupTask() {
+		// Check if task is already running
+		if (cleanupTask != null && !cleanupTask.isCancelled()) return;
+		
+		// Setup periodic runnable to unload each entity's last active region after `inactiveRegionLoadTime` of inactivity
+		cleanupTask = new BukkitRunnable() {
+			@Override
+			public void run() {
+				long now = world.getGameTime();
+				
+				// Enumerate previously active entities and the time of their last activity
+				entityActiveTimes.entrySet().removeIf(entry -> {
+					long entityActiveTime = entry.getValue();
+					if ((now - entityActiveTime) < inactiveRegionLoadTime) return false;
+					
+					// Entity was inactive for longer than `inactiveRegionLoadTime`, remove last active region and forget entity
+					ChunkCoord coord = entityActiveRegions.remove(entry.getKey());
+					removeRegion(coord);
+					return true;
+				});
+				
+				// Stop task while there aren't any active entities anymore
+				if (entityActiveTimes.isEmpty()) {
+					cleanupTask.cancel();
+				}
+			}
+		}.runTaskTimer(plugin, updateInterval, updateInterval);
 	}
 }
